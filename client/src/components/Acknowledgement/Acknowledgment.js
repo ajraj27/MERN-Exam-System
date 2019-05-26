b367736dca25acbf4f969e5559d0f4b476348630
ajraj27 <rajanuj.iiita@gmail.com>
2019-05-26 15:39:11 +0530

    some style changes

--- client/src/components/Acknowledgement/Acknowledgment.js
+++ client/src/components/Acknowledgement/Acknowledgment.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom';
 const Acknowledgement=() => (
             <div className="row bg-danger" style={{height:"100vh"}}>
                 <div className="col-md-8 offset-md-2">
-                    <div className="card mt-5 bg-success ">
+                    <div className="card mt-5 ">
                         <div className="card-body">
                             <h1>Response submitted successfully!</h1>
-                            <Link className="text-white" to="/">See previous response</Link>
+                            <Link to="/">See previous response</Link>
                         </div>
                     </div>
                 </div>
